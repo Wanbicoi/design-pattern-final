5d412040dc97155db9a9ec77c1b5f310360acde3
chancholat <chanhainguyen97@gmail.com>
2025-01-12 17:29:03 +0700

    add singleton to IoC

--- ReadDBSchema/GenericForm/MainWindow.cs
+++ ReadDBSchema/GenericForm/MainWindow.cs
@@ -6,48 +6,19 @@ namespace GenericForm
 {
     public partial class MainWindow : Form
     {
-        private SimpleIoCContainer _container;
-        //private readonly Dictionary<string, Type> formTypes = new Dictionary<string, Type>()
-        //{
-        //    { "Users", typeof(ModelForms.User) },
-        //    { "Products", typeof(ModelForms.Product) },
-        //    //{ "Clients", typeof(ModelForms.Clients) },
-        //};
+
         private readonly Dictionary<string, Type> _formTypes = ModelFormMapper.FormTypes;
         private readonly Dictionary<Type, Type> _modelFormMapping = ModelFormMapper.ModelFormMapping;
-
-        //private Dictionary<Type, Type> _modelFormMapping = new Dictionary<Type, Type>
-        //{
-        //    //{ typeof(DBContext.clients), typeof(ModelForms.Clients) },
-        //    { typeof(DBContext.Product), typeof(ModelForms.Product) },
-        //};
-
 
         public MainWindow(string databaseType, string connectionString)
         {
             InitializeComponent();
-            //RegistDependencies(databaseType, connectionString);
             DynamicRegistDependencies(databaseType, connectionString);
         }
 
-        //private void RegistDependencies(string databaseType, string connectionString)
-        //{
-        //    _container = new SimpleIoCContainer();
-
-        //    _container.Register<BaseApplicationDbContext<clients>>(() =>
-        //       new BaseApplicationDbContext<clients>(databaseType, connectionString));
-
-        //    _container.Register<Clients>(() =>
-        //    {
-        //        var dbContext = _container.Resolve<BaseApplicationDbContext<clients>>();
-        //        return new Clients(dbContext);
-        //    });
-
-        //}
 
         private void DynamicRegistDependencies(string databaseType, string connectionString)
         {
-            _container = new SimpleIoCContainer();
 
             foreach (var mapping in _modelFormMapping)
             {
@@ -59,7 +30,7 @@ namespace GenericForm
                     .GetMethod(nameof(SimpleIoCContainer.Register))
                     ?.MakeGenericMethod(typeof(BaseApplicationDbContext<>).MakeGenericType(modelType));
 
-                dbContextRegistrationMethod?.Invoke(_container, new object[]
+                dbContextRegistrationMethod?.Invoke(SimpleIoCContainer.Instance, new object[]
                 {
                     (Func<object>)(() => Activator.CreateInstance(typeof(BaseApplicationDbContext<>).MakeGenericType(modelType), databaseType, connectionString)),
                     null
@@ -70,11 +41,11 @@ namespace GenericForm
                     .GetMethod(nameof(SimpleIoCContainer.Register))
                     ?.MakeGenericMethod(clientType);
 
-                serviceRegistrationMethod?.Invoke(_container, new object[]
+                serviceRegistrationMethod?.Invoke(SimpleIoCContainer.Instance, new object[]
                 {
                     (Func<object>)(() =>
                     {
-                        var dbContext = _container.Resolve(typeof(BaseApplicationDbContext<>).MakeGenericType(modelType));
+                        var dbContext = SimpleIoCContainer.Instance.Resolve(typeof(BaseApplicationDbContext<>).MakeGenericType(modelType));
                         return Activator.CreateInstance(clientType, dbContext);
                     }),
                     null
@@ -91,12 +62,11 @@ namespace GenericForm
         {
             if (listBox1.SelectedItem != null && _formTypes.TryGetValue(listBox1.SelectedItem.ToString()!, out Type formType))
             {
-                //Form form = (Form)Activator.CreateInstance(formType)!;
-                //form.ShowDialog();
+  
                 if (formType != null)
                 {
-                    // Resolve the form dynamically using the type
-                    var form = _container.Resolve(formType) as Form;
+
+                    var form = SimpleIoCContainer.Instance.Resolve(formType) as Form;
                     if (form != null)
                     {
                         form.ShowDialog();
